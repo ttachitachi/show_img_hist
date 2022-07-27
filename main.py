9be3331dbe353be86e20d27d2f95e98a4c6b4c99
Yuji Tachi <y.tachi.882@stn.nitech.ac.jp>
2022-07-28 05:17:26 +0900

    modify code3

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from skimage.io import imread
 
 # download the image
 img_url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/06/Sattatouge_mtfuji.jpg/1280px-Sattatouge_mtfuji.jpg'
-im = imread(img_url)
+im = imread(img_url))
 
 st.image(im, caption='image from wikimedia commons',
          use_column_width=True)
